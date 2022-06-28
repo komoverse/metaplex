1741e459a7e278adf96535814f51edd5e4fa362c
nikkoagustino <nikkoagustino@gmail.com>
2022-06-28 13:15:07 +0700

    remove strict mode

--- js/packages/candy-machine-ui/src/index.tsx
+++ js/packages/candy-machine-ui/src/index.tsx
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
 
